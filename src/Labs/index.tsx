11411e7a217e7c344cf2b54452c2967319ddfdf9
Bhuvan <bhuvan@Bhuvans-MacBook-Air.local>
2024-09-19 10:07:59 -0400

    Final Assignment work

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,11 +2,11 @@ import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
-import Lab3 from "./Lab3"; 
+import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>BHUVAN NALLAMOTHU</h1>
+      <h1>NALLAMOTHU BHUVAN</h1>
       <h4>Web Development | Section: 03 | CRN: 20596 | Online</h4>
       <h4>Github (Links to all relevant source code repositories) :<a href="https://github.com/iambhuvan/kanbas-react-web-app">LINK</a></h4>
       <h4>Labs:</h4>
@@ -16,7 +16,7 @@ export default function Labs() {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
-        </Routes>
+      </Routes>
     </div>
   );
 }
