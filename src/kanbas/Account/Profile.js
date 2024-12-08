be182822b5ac3d6de8a1e1205df8889f994bb834
Bhuvan <bhuvan@Bhuvans-MacBook-Air.local>
2024-12-08 18:20:38 -0500

    A6

--- src/kanbas/Account/Profile.js
+++ src/kanbas/Account/Profile.js
@@ -1,9 +1,9 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-// import { setCurrentUser } from "./reducer";
-import { setCurrentUser } from "./reducer";
 import * as client from "./client";
+import { setCurrentUser } from "./reducer.js";
+
 
 export default function Profile() {
   const [profile, setProfile] = useState({});
